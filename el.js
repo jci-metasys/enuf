fc5fc423f590bbefc1aee7832aa6f6fffb07f206
Michael Welch <michael.g.welch@jci.com>
2020-07-19 14:53:01 -0500

    refactor getEnums

--- el.js
+++ el.js
@@ -1,43 +1,67 @@
 const fs = require("fs")
-const _ = require("lodash")
+const colors = require("colors/safe")
 const path = require("path")
+const _ = require("lodash")
+const { table } = require("table")
+const { set } = require("lodash")
+
 
 /* global process */
 /* eslint-disable no-console */
 
 const installDir = path.dirname(process.argv[1])
 const enumsFile = path.join(installDir, "data", "enums.json")
+const translationsFile = path.join(installDir, "data", "translations.json")
+
+function getTranslations() {
+    return JSON.parse(fs.readFileSync(translationsFile, { encoding: "utf8" }))
+}
+
+function addIdAndNameToSet(set, key) {
+    const [name, id] = key.split(".")
+    return { ...set, id, name }
+}
+
+function getEnums() {
+    const enumsByKey = JSON.parse(fs.readFileSync(enumsFile, { encoding: "utf8" }))
+
+    const enumsByName = _.chain(enumsByKey)
+        .mapValues(addIdAndNameToSet)
+        .mapKeys(set => set.name)
+        .value()
+
+    const enumsById = _.mapKeys(enumsByName, set => set.id)
+
+    return { enumsByName, enumsById, enumsByKey }
+}
 
 function complete([partialSetName, partialMemberName]) {
-    const allEnums = JSON.parse(fs.readFileSync(enumsFile, { encoding: "utf-8" }))
-    const setKeys = _.keys(allEnums)
-    const setNames = _.map(setKeys, setKey => setKey.split(".")[0])
 
-    const namesToKeys = _.zipObject(setNames, setKeys)
+    const { enumsByName } = getEnums()
 
     if (!partialSetName && !partialMemberName) {
-        return setNames
+        return _.keys(enumsByName)
     }
 
     if (partialSetName) {
         const setMatches = _.filter(
-            setNames,
+            _.keys(enumsByName),
             setName => _.startsWith(setName, partialSetName),
         )
 
         if (!partialMemberName) {
             if (setMatches.length === 1 && setMatches[0] === partialSetName) {
-                const setKey = namesToKeys[partialSetName]
+                const setName = partialSetName
 
-                const { members } = allEnums[setKey]
+                const { members } = enumsByName[setName]
 
                 return _.map(_.values(members), memberKey => memberKey.split(".")[1])
             }
             return setMatches
         } else {
             const setName = partialSetName
-            const setKey = namesToKeys[setName]
-            const memberNames = _.map(_.values(allEnums[setKey].members),
+
+            const memberNames = _.map(_.values(enumsByName[setName].members),
                 memberKey => memberKey.split(".")[1])
 
             const matches = _.filter(memberNames,
@@ -52,59 +76,66 @@ function complete([partialSetName, partialMemberName]) {
     }
 }
 
-function main() {
+function findSet({ enumsByName, enumsById }, setArg) {
+
+    var set = enumsByName[setArg]
+    if (set) {
+        return set
+    }
+
+    return enumsById[setArg]
+}
+
+function findMember(members, memberArg) {
+    if (members[memberArg]) {
+        return [memberArg, members[memberArg]]
+    }
+
+    return [_.findKey(members, value => value.split(".")[1] === memberArg), memberArg]
+}
+
+// function printEnumSet({enumsByName, enumsById}, translations, enumSet) {
+//     console.log(colors.green(colors.bold(`${enumSet.description} (${enumSetName},${enumSet.id})`)))
+//     const data = [
+//         [colors.bold("Name"), colors.bold("Id"), colors.bold("Description")],
+//     ]
+
+//     const members = _.chain(enumSet.members)
+//         .toPairs()
+//         .map(pair => [`${enumSetName}.${pair[0]}`, pair[1].id, pair[1].description])
+//         .sortBy(triple => triple[1])
+//         .value()
+
+//     members.forEach(member => data.push(member))
+
+//     const output = table(data)
+//     console.log(output)
+// }
+
+function main([setArg, memberArg]) {
     if (process.stdout.isTTY) {
         colors.enable()
     } else {
-    // output is going to a file, disable color markers
+        // output is going to a file, disable color markers
         colors.disable()
     }
 
-    const userArgs = parseArgs(process.argv.slice(2), {})
+    if (setArg) {
 
-    if (userArgs._[0] && userArgs._[0] === "complete") {
-        console.log(_.join(complete(userArgs), " "))
-        return;
-    }
+        const enums = getEnums()
 
-    const allEnums = JSON.parse(fs.readFileSync("allEnums.json", { encoding: "utf8" }))
+        // const {setName, setKey, set} = findSet(setArg, enums)
+        // if (set) {
+        //     const translations = getTranslations()
+        //     if (memberArg) {
+        //         const [memberId, memberShortName] = findMember(set.members, memberArg)
+        //     } else {
+        //         printEnumSet(enums, translations, set)
+        //     }
 
-    switch (userArgs._.length) {
-    case 1: {
-        const setArg = userArgs._[0]
-        const setIdArg = parseInt(setArg, 10)
-        const setNameArg = _.isNaN(setIdArg) ? setArg : undefined
-        const { setName, set } = findSet(allEnums, setNameArg, setIdArg)
-        if (set) {
-            printEnumSet(setName, set)
-        } else {
-            console.log(`Set '${setArg}' not found.`)
-        }
-        return
-    }
-    case 2: {
-        const setArg = userArgs._[0]
-        const setIdArg = parseInt(setArg, 10)
-        const setNameArg = _.isNaN(setIdArg) ? setArg : undefined
-        const { setName, set } = findSet(allEnums, setNameArg, setIdArg)
-
-        if (set) {
-            const memberArg = userArgs._[1]
-            const memberIdArg = parseInt(memberArg, 10)
-            const memberNameArg = _.isNaN(memberIdArg) ? memberArg : undefined
-            const { memberName, member } = findMember(set, memberNameArg, memberIdArg)
-
-            if (member) {
-                printEnumMember(setName, set, memberName, member)
-            }
-            console.log(`Set '${setName}' found but member with name or id '${memberArg}' not found`)
-        } else {
-            console.log(`Set '${setArg}' not found.`)
-        }
-        break
-    }
-    default:
-        break
+        // } else {
+        //     console.error("Set `${setArg}` not found")
+        // }
     }
 }
 
