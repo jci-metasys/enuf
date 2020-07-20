78da115f94d56dee6c5476edc1f6d116482ba38b
Michael Welch <michael.g.welch@jci.com>
2020-07-20 10:26:33 -0500

    Remove the unused extra arg in the call to `complete`

--- enul-completion.bash
+++ enul-completion.bash
@@ -1,7 +1,7 @@
 
 function _enul() {
   #COMPREPLY='attributeEnumSet'
-  COMPREPLY=($(enul complete "$COMP_LINE" $COMP_LINE));
+  COMPREPLY=($(enul complete $COMP_LINE));
 }
 
 #alias enul=$HOME/repos/enul/index.js
